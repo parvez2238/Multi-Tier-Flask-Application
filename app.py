fcce5a4230ffc2e4cddf9e6aaa71bf067c0cb4e2
parvez2238 <parvezrahaman2238@gmail.com>
2023-11-05 00:26:23 +0530

    Updated Documentions

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-#This file will contain your Flask server code
+#This file will contain Flask server code
 from flask import Flask, request, jsonify, render_template
 import mysql.connector
 import os
@@ -8,18 +8,18 @@ app = Flask(__name__, static_folder='static')
 MYSQL_HOST = os.getenv('MYSQL_HOST', 'mysql') # Get MySQL host from environment variable (default to 'mysql' if not set)
 
 db = mysql.connector.connect(
-    host= MYSQL_HOST,  # Use "localhost" if your database is on the same machine. If not, replace with the actual host address.
-    user="root",       # Replace with your MySQL username.
-    password="Parvez@2238",  # Replace with your MySQL password.
-    database="User_Form"  # Replace with your MySQL database name.
+    host= MYSQL_HOST,  # MYSQL is running on container default host is MYSQL_HOST(if it is local mechine use localhost)
+    user="root",       # MySQL username.(default root)
+    password="Parvez@2238",  # MySQL password.
+    database="User_Form"  # MySQL database name. (Create database)
 )
 cursor = db.cursor()
 
-TABLE_NAME = "User_Data"  # Replace with your MySQL table name.
+TABLE_NAME = "User_Data"  # MySQL table name.(Create the table in database)
 
 @app.route('/')
 def index():
-    print("Request received!")  # Add this line for debugging
+    print("Request received!")  #debugging
     return render_template('index.html')
 
 # Route to handle form submissions
@@ -43,7 +43,8 @@ def save_data():
         print(str(e))
         response = {"error": "Internal Server Error"}
         return jsonify(response), 500
-
+    
+# Route for getting data from database
 @app.route('/api/data', methods=['GET'])
 def get_data():
     try:
@@ -56,18 +57,19 @@ def get_data():
         print("Error:", str(e))
         response = {"error": "Internal Server Error"}
         return jsonify(response), 500
-
+    
+#Display the data in the port 5000/data
 @app.route('/data')
 def data_page():
     try:
         cursor.execute("SELECT * FROM User_Data")
         data = cursor.fetchall()
-        response_data = [{"name": row[1], "email": row[2]} for row in data]
+        response_data = [{"name": row[1], "email": row[2]} for row in data] #row 0 is my PK| row 1 is name| row 2 is email)
         return render_template('data.html', data=response_data)
     except Exception as e:
         print("Error:", str(e))
 
-
+#initialize the port, debug is on for troubleshooting
 if __name__ == '__main__':
     app.run(debug=True,host='0.0.0.0', port=5000)
 
